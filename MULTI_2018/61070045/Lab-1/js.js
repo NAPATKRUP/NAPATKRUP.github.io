e3326198cf5ebefac5508a64cef1d44122dc1536
Napat Arayawattanapong <napatkrup@hotmail.com>
2019-01-20 22:17:50 +0700

    Edit my web

--- MULTI_2018/61070045/Lab-1/js.js
+++ MULTI_2018/61070045/Lab-1/js.js
@@ -3,51 +3,50 @@ const word = document.querySelector('#word').innerHTML;
 document.querySelector('#word').innerHTML = 'Hi!';
 let word_length = word.length;
 for (var i = 3; i < word_length; i++) {
-	let time = i*75;
-	let char = word[i];
-	setTimeout(function(){
-		document.querySelector('#word').innerHTML = document.querySelector('#word').innerHTML+char;
-	},600+time);
+    let time = i*75;
+    let char = word[i];
+    setTimeout(function(){
+        document.querySelector('#word').innerHTML = document.querySelector('#word').innerHTML+char;
+    },600+time);
 }
 
-//My ball
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+//My Snow
+const colors = ["#FFFFFF"];
 
-const numBalls = 75;
-const balls = [];
+const numSnows = 100;
+const snows = [];
 
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
+for (let i = 0; i < numSnows; i++) {
+    let snow = document.createElement("div");
+    snow.classList.add("snow");
+    snow.style.background = colors[Math.floor(Math.random() * colors.length)];
+    snow.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    snow.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    snow.style.transform = `scale(${Math.random()})`;
+    snow.style.width = `${Math.random()}em`;
+    snow.style.height = snow.style.width;
 
-  balls.push(ball);
-  document.body.append(ball);
+    snows.push(snow);
+    document.body.append(snow);
 }
 
-// Key frames
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  }
-
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
+snows.forEach((el, i, ra) => {
+    let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12
     }
-  );
+
+    let anim = el.animate(
+        [
+            { transform: "translate(0, 0)" },
+            { transform: `translate(${to.x}rem, ${to.y}rem)` }
+        ],
+        {
+        duration: (Math.random() + 1) * 2000,
+        direction: "alternate",
+        fill: "both",
+        iterations: Infinity,
+        easing: "ease-in-out"
+        }
+    );
 });
